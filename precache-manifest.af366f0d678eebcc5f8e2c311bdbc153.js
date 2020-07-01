23927ee236d155491b696894e8fe2491857dda41
mickies-dairy-bar <mickiesdairybar@gmail.com>
2020-06-30 17:11:58 -0700

    Updates

---
+++ precache-manifest.af366f0d678eebcc5f8e2c311bdbc153.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3050863dbf2841ee06cd68c9076755ab",
+    "url": "/mickies-website/index.html"
+  },
+  {
+    "revision": "f0ad859344b18e312488",
+    "url": "/mickies-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "252707e1229efc0d551a",
+    "url": "/mickies-website/static/js/2.c99ee234.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mickies-website/static/js/2.c99ee234.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0ad859344b18e312488",
+    "url": "/mickies-website/static/js/main.36289bb8.chunk.js"
+  },
+  {
+    "revision": "d65deee7f3ee4057aac1",
+    "url": "/mickies-website/static/js/runtime-main.5a721394.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mickies-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
